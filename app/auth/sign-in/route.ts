67d1cce85e66735672c4d96cdbfd270509f3a9d1
Aleksandr Skakovskiy <optimusway@gmail.com>
2023-08-07 02:19:54 +0500

    feat: add google authentication

--- app/auth/sign-in/route.ts
+++ app/auth/sign-in/route.ts
@@ -1,20 +1,54 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export async function POST(request: Request) {
-  const requestUrl = new URL(request.url)
-  const formData = await request.formData()
-  const email = String(formData.get('email'))
-  const password = String(formData.get('password'))
-  const supabase = createRouteHandlerClient({ cookies })
+  const requestUrl = new URL(request.url);
+
+  const formData = await request.formData();
+
+  const email = String(formData.get("email"));
+  const password = String(formData.get("password"));
+  const provider = String(formData.get("provider"));
+
+  const supabase = createRouteHandlerClient({ cookies });
+
+  if (provider === "google") {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+        redirectTo: `${requestUrl.origin}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      console.error(error.message);
+
+      return NextResponse.redirect(
+        `${requestUrl.origin}/login?error=Could not authenticate user`,
+        {
+          // a 301 status is required to redirect from a POST to a GET route
+          status: 301,
+        }
+      );
+    }
+
+    return NextResponse.redirect(data.url, {
+      // a 301 status is required to redirect from a POST to a GET route
+      status: 301,
+    });
+  }
 
   const { error } = await supabase.auth.signInWithPassword({
     email,
     password,
-  })
+  });
 
   if (error) {
     return NextResponse.redirect(
@@ -23,11 +57,11 @@ export async function POST(request: Request) {
         // a 301 status is required to redirect from a POST to a GET route
         status: 301,
       }
-    )
+    );
   }
 
   return NextResponse.redirect(requestUrl.origin, {
     // a 301 status is required to redirect from a POST to a GET route
     status: 301,
-  })
+  });
 }
