7a297bf20599f571e8ebab5d3585539af46494e4
Aleksandr Skakovskiy <optimusway@gmail.com>
2023-08-07 02:48:03 +0500

    style: add google icon

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Messages from "./messages";
+import { Icons } from "@/components/icons";
 
 export default function Login() {
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-8">
       <Link
         href="/"
         className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
@@ -26,7 +27,7 @@ export default function Login() {
       </Link>
 
       <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+        className="flex flex-col w-full justify-center gap-2 text-foreground"
         action="/auth/sign-in"
         method="post"
       >
@@ -54,20 +55,30 @@ export default function Login() {
         </button>
         <button
           formAction="/auth/sign-up"
-          className="border border-gray-700 rounded px-4 py-2 text-black dark:text-white mb-2"
+          className="border border-gray-700 rounded px-4 py-2 text-black dark:text-white"
         >
           Sign Up
         </button>
         <Messages />
       </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-sm">
+          <span className="bg-background text-foreground px-2">
+            or continue with
+          </span>
+        </div>
+      </div>
       <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+        className="flex flex-col w-full justify-center gap-2 text-foreground"
         action="/auth/sign-in"
         method="post"
       >
         <input type="hidden" name="provider" value="google" />
-        <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
-          Sign In with Google
+        <button className="border border-gray-700 rounded px-4 py-2 text-black dark:text-white flex items-center justify-center">
+          <Icons.Google className="mr-2 h-4 w-4" /> Google
         </button>
       </form>
     </div>
